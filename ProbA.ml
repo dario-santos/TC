5f5a395c2521f7664119394562e19a29463468dc
Dário Santos <dariovfsantos@gmail.com>
2020-02-26 22:40:30 +0000

    Changed for loops for iteri

--- ProbA.ml
+++ ProbA.ml
@@ -10,15 +10,13 @@ let min i j =
   let m = if get i (j-1) <= get (i-1) (j-1) then get i (j-1) else get (i-1) (j-1) in
   if m <= get (i-1) j then m else get (i-1) j
 
-let operation dna1 dna2 n m = 
-  for i = 0 to n - 1 do
-    for j = 0 to m - 1 do
-        matriz.(i).(j) <- if dna1.[i] = dna2.[j] then get (i-1) (j-1) else (min i j) + 1;
-      done;
-  done
+let operation i j = 
+  matriz.(i).(j) <- if dna1.[i] = dna2.[j] then get (i-1) (j-1) else (min i j) + 1
 
-let main dna1 dna2 n m =
-  if n == 0 then m else if m == 0 then n 
-  else let _= operation dna1 dna2 n m in matriz.(n - 1).(m - 1)
+let main = function
+  | 0, _ -> m | _, 0 -> n
+  | _,_ ->  
+    Array.iteri (fun i a -> (Array.iteri (fun j _ -> operation i j) a)) matriz;
+    matriz.(n - 1).(m - 1)
 
-let _ = Printf.printf "%d\n" (main dna1 dna2 n m)
+let _ = Printf.printf "%d\n" (main (n,m))
