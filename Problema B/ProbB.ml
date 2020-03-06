030946edf8cb02b827c4cc787bb1cc1a43da59c7
Dário Santos <dariovfsantos@gmail.com>
2020-03-06 16:53:36 +0000

    Added a seccond implementation of the B problem

--- Problema B/ProbB.ml
+++ Problema B/ProbB.ml
@@ -1,37 +1,30 @@
-let dna1, dna2 = read_line(), read_line()
-let n, m = String.length dna1, String.length dna2
+let dna1, n = let s = read_line() in s, String.length s
+let dna2, m = let s = read_line() in s, String.length s
 let matriz = Array.make_matrix n m 0
+let min = ref 0
 
-let min, out = ref 0, ref ""
-
-let operation i a = 
+let get i j = try matriz.(i-1).(j-1) + 1 with _ -> 1
+let operation i c =
   for j = 0 to m - 1 do
-    let tmp = if i - 1 < 0 || j - 1 < 0 then 0 else matriz.(i-1).(j-1) in
-    (if dna1.[i] = dna2.[j] then matriz.(i).(j) <- tmp + 1); 
-    if matriz.(i).(j) > !min then
-      (min := matriz.(i).(j);
-      out := string_of_int !min)
-    else if matriz.(i).(j) = !min then
-      out := !out ^ " " ^ (string_of_int !min)
+    if c = dna2.[j] then matriz.(i).(j) <- get i j;
+    if matriz.(i).(j) > !min then min := matriz.(i).(j)
   done
 
-let main = function
-  | 0, _ | _, 0 -> "0"
-  | _ -> 
-    String.iteri (fun i a -> operation i a) dna1;
-    !out
+let _ = 
+  match (n,m) with
+  | 0, _ | _, 0 -> ()
+  | _ -> String.iteri (fun i c -> operation i c) dna1;
+  Printf.printf "%d\n" !min
 
-let _ = Printf.printf "%s\n" (main (n,m))
-
-let debug () = 
+let debug () =
   for i = 0 to n-1 do
     for j = 0 to m-1 do
       Printf.printf "%d " matriz.(i).(j)
     done;
-    Printf.printf "\n"
+    Printf.printf "\n";
   done
 
-let _ = debug()
+(*let _ = debug ()*)
 
 (*
   o segundo problema trata de descobrir qual é a maior parte comum entre dois ramos de ADN
@@ -50,5 +43,5 @@ let _ = debug()
   ABCABC
 
   Output:
-  2 2
+  2
 *)
