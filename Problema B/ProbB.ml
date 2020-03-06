d304828b923f3e03e1daffd30d74cd935092e5da
Dário Santos <dariovfsantos@gmail.com>
2020-03-06 15:55:40 +0000

    B is done

--- Problema B/ProbB.ml
+++ Problema B/ProbB.ml
@@ -1,18 +1,27 @@
-(* o segundo problema trata de descobrir qual é a maior parte comum entre dois ramos de ADN *)
 let dna1, dna2 = read_line(), read_line()
 let n, m = String.length dna1, String.length dna2
-
 let matriz = Array.make_matrix n m 0
 
-let get i j = if i - 1 < 0 || j - 1 < 0 then 0 else matriz.(i-1).(j-1)
+let min, out = ref 0, ref ""
 
-let init () = 
-  for i = 0 to n-1 do
-    for j = 0 to m-1 do
-      if dna1.[i] = dna2.[j] then 
-        matriz.(i).(j) <- (get i j) + 1
-    done;
+let operation i a = 
+  for j = 0 to m - 1 do
+    let tmp = if i - 1 < 0 || j - 1 < 0 then 0 else matriz.(i-1).(j-1) in
+    (if dna1.[i] = dna2.[j] then matriz.(i).(j) <- tmp + 1); 
+    if matriz.(i).(j) > !min then
+      (min := matriz.(i).(j);
+      out := string_of_int !min)
+    else if matriz.(i).(j) = !min then
+      out := !out ^ " " ^ (string_of_int !min)
   done
+
+let main = function
+  | 0, _ | _, 0 -> "0"
+  | _ -> 
+    String.iteri (fun i a -> operation i a) dna1;
+    !out
+
+let _ = Printf.printf "%s\n" (main (n,m))
 
 let debug () = 
   for i = 0 to n-1 do
@@ -22,40 +31,24 @@ let debug () =
     Printf.printf "\n"
   done
 
-let main() =
-  init();
-  let a = ref 0 in
-  let b = ref 0 in
-  let k = ref 0 in
-  for i = 0 to n-1 do
-    for j = 0 to m-1 do
-      if matriz.(i).(j) > !k then 
-        k := matriz.(i).(j);
-        a := i;
-        b := j;
-    done;
-  done;
-  !a,!b,!k
-
-(* 
-  a = Inicio do padrão DNA1
-  b = Inicio do padrão DNA2 
-  k = Tamanho do padrão
-  Output: a b k
-*)
-let _ = let a,b,k = main() in Printf.printf "%d %d %d\n" a b k
+let _ = debug()
 
 (*
+  o segundo problema trata de descobrir qual é a maior parte comum entre dois ramos de ADN
 
-   BAAAA
-  A01111
-  A01222
-  B10000
-  B10000
-
+   ABCABC
+  A100100
+  B020020
+  C003003
+  
   Função de inicialização:
 
   Se dna1.[i] = dna2.[j] então matriz.(i).(j) <- matriz(i-1).(j-1) + 1
 
+  Input: 
+  ABC
+  ABCABC
 
+  Output:
+  2 2
 *)
