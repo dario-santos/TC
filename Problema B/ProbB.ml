f249badb736d78e96ba4dcebc0e467ff1c297c91
Dário Santos <dariovfsantos@gmail.com>
2020-03-08 18:18:23 +0000

    improved problem B efficiency and tests

--- Problema B/ProbB.ml
+++ Problema B/ProbB.ml
@@ -3,17 +3,17 @@ let dna2, m = let s = read_line() in s, String.length s
 let matriz = Array.make_matrix n m 0
 let min = ref 0
 
-let get i j = try matriz.(i-1).(j-1) + 1 with _ -> 1
-let operation i c =
+let iterate i c =
   for j = 0 to m - 1 do
-    if c = dna2.[j] then matriz.(i).(j) <- get i j;
-    if matriz.(i).(j) > !min then min := matriz.(i).(j)
+    if c = dna2.[j] then(
+      matriz.(i).(j) <- (try matriz.(i-1).(j-1) + 1 with _ -> 1);
+      if matriz.(i).(j) > !min then min := matriz.(i).(j))
   done
 
-let _ = 
-  match (n,m) with
+let _ =
+  (match (n,m) with
   | 0, _ | _, 0 -> ()
-  | _ -> String.iteri (fun i c -> operation i c) dna1;
+  | _ -> String.iteri (fun i c -> iterate i c) dna1);
   Printf.printf "%d\n" !min
 
 let debug () =
--- Problema B/run_tests.sh
+++ Problema B/run_tests.sh
@@ -7,11 +7,14 @@ echo "TC - Problema B - (ficheiros em tests/)"
 
 for f in tests/*.txt; do
     max_i=`expr $max_i + 1`;
-    echo $f
     expected=tests/`basename $f .txt`.out
     rm -f out
+    start=`date +%s000`
     if (./a.out < $f) > out; then
 	if cmp --quiet out $expected; then
+        end=`date +%s000`
+        elapsed=$(($end-$start))
+        echo "$f - Time elapsed: $elapsed"
 	    score_i=`expr $score_i + 1`;
 	else
 	    echo "  FALHA : saida errada para $f"
@@ -21,9 +24,6 @@ for f in tests/*.txt; do
     fi
 done
 
-end=`date +%s000`
-elapsed=$(($end-$start))
 echo
 percent=`expr 100 \* $score_i / $max_i`;
 echo "Score: $score_i / $max_i testes, seja $percent%"
-echo "Time elapsed: $elapsed"
