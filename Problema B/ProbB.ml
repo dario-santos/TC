4475562ac702ce26cc57551e9092fa7d8ef7a312
Dário Santos <dariovfsantos@gmail.com>
2020-04-08 18:38:55 +0100

    Resolvido o Test set 1 do problema B
    Test set 2 -> Suffix Trees

--- Problema B/ProbB.ml
+++ Problema B/ProbB.ml
@@ -3,44 +3,19 @@ let dna2, m = let s = read_line() in s, String.length s
 let matriz = Array.make_matrix 2 n 0
 let min = ref 0
 
-let iterate arr c =
+let iterate i c =
+  let arr = matriz.(i mod 2) in
   for j = 0 to n-1 do
     if c = dna1.[j] then
-      (arr.(j) <- (try arr.(j-1) + 1 with _ -> 1);
+      (arr.(j) <- (try matriz.((i-1) mod 2).(j-1) + 1 with _ -> 1);
       if arr.(j) > !min then min := arr.(j))
+    else 
+      arr.(j) <- 0;
   done
 
 let _ =
   (match (n,m) with
   | 0, _ | _, 0 -> ()
   | _ when String.equal dna1 dna2 -> min := n
-  | _ -> String.iteri (fun i c -> iterate matriz.(i mod 2) c) dna2);
+  | _ -> String.iteri (fun i c -> iterate i c) dna2);
   Printf.printf "%d\n" !min
-
-(*
-  # Problema A e Problema B
-
-  À semelhança do problema A, o problema B também pede por uma resposta ótima.
-  Logo utilizámos programação dinamica para resolver este problema de forma eficiente.
-
-  Também à semelhança do problema A, alocámos apenas uma array com 2n elementos.
-
-  # Algoritmo
-
-    |A|C|A|A|T|
-  --+-+-+-+-+-+
-   C|0 1 0 0 0
-   A|1 0 2 1 0
-   T|0 0 0 0 2
-  
-  # Algumas Considerações:
-
-  Neste caso temos duas situações que produzem o mesmo tamanho máximo: 
-    
-    - dna1: CA,  dna2: ACA
-    - dna1: CAT, dna2: ACAAT
-
-  Visto que não importa qual o que retornamos, foi decidido retornar o primeiro
-  de forma a otimizar o algoritmo.
-
-*)
